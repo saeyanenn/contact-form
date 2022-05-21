ea8958206a50a280d7b1437742b7f422bbc6638a
ito sae <saeito20040122@gmail.com>
2022-05-21 13:41:04 +0900

    コンタクトフォームを作成

---
+++ form.php
@@ -0,0 +1,67 @@
+<?php
+
+$input_array = [
+    "name" => [
+        "label" => "お名前",
+        "type" => "text",
+        "placeholder" => "山田太郎",
+        "required" => true
+    ],
+    "email" => [
+        "label" => "メールアドレス",
+        "type" => "email",
+        "placeholder" => "example@example.com",
+        "required" => true
+    ],
+    "department" => [
+        "label" => "学部・学科名",
+        "type" => "text",
+        "placeholder" => "工学部電気電子工学科",
+        "required" => false
+    ],
+    "tel" => [
+        "label" => "電話番号",
+        "type" => "tel",
+        "placeholder" => "090-1234-5678",
+        "required" => false
+    ],
+];
+
+$subject_array = [
+    "授業",
+    "サークル",
+    "アルバイト",
+    "就職活動",
+    "その他"
+];
+?>
+
+<form action="" method="POST">
+    <?php foreach ($input_array as $key => $value) { ?>
+        <div class="form-wrapper">
+            <label for=<?= $key; ?>><?= $value["label"]; ?></label>
+            <?php if ($value["required"]) : ?>
+                <span class="required">必須</span>
+            <?php endif; ?>
+            <input id=<?= $key; ?> type=<?php echo $value["type"]; ?> name=<?= $key; ?> placeholder=<?= "例）&nbsp;" . $value["placeholder"] ?> required="<?= $value["required"]; ?>">
+        </div>
+    <?php } ?>
+    <div class="form-wrapper">
+        <label for="subject">件名</label>
+        <select name="subject" id="subject">
+            <?php foreach ($subject_array as $value) { ?>
+                <option value=<?= $value; ?>><?= $value; ?></option>
+            <?php } ?>
+        </select>
+    </div>
+    <div class="form-wrapper">
+        <label for="content">お問い合わせ内容</label>
+        <textarea name="content" id="content" cols="30" rows="10"></textarea>
+    </div>
+    <div class="button-wrapper">
+        <button type="submit">
+            送信
+        </button>
+    </div>
+
+</form>
