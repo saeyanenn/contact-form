abc31eb3aadddc113019437b317c686bfa99fbbd
ito sae <saeito20040122@gmail.com>
2022-05-21 14:38:11 +0900

    バリデーションメッセージが消えるようにした

--- formValidation.js
+++ formValidation.js
@@ -2,20 +2,31 @@ $(function () {
     $("#name").on("blur", function(event){
         const nameLength = event.target.value.length;
         if(nameLength > 20){
-            $("#name-validation").text("お名前は20文字以内で入力してください")
+            $("#name-validation").text("お名前は20文字以内で入力してください");
+        }
+        else{
+            $("#name-validation").text("");
+
         }
     });
     
     $("#email").on("blur", function(event){
         if(!/^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]+.[A-Za-z0-9]+$/.test(event.target.value)){
-            $("#email-validation").text("メールアドレス形式で入力してください")
+            $("#email-validation").text("メールアドレス形式で入力してください");
+        }
+        else{
+            $("#email-validation").text("");
         }
     });
 
     $("#department").on("blur", function(event){
         const nameLength = event.target.value.length;
         if(nameLength > 20){
-            $("#department-validation").text("学部学科名は20文字以内で入力してください")
+            $("#department-validation").text("学部学科名は20文字以内で入力してください");
+        }
+        else {
+            $("#department-validation").text("");
+
         }
     });
 
@@ -26,16 +37,23 @@ $(function () {
             $("#tel-validation").text("電話番号にハイフンを含めないでください");
             return;
         }
-        if(telLength !== 10 || telLength !== 10){
-            $("#tel-validation").text("電話番号は10文字または11文字で入力してください")
+        if(telLength !== 10 || telLength !== 11){
+            $("#tel-validation").text("電話番号は10文字または11文字で入力してください");
+            return;
         }
+        $("#tel-validation").text("");
+        
     });
     
     $("#content").on("blur", function(event){
         const contentLength = event.target.value.length;
         console.log(contentLength);
         if(contentLength > 1000){
-            $("#content-validation").text("お問い合わせ内容は1000文字以内で入力してください")
+            $("#content-validation").text("お問い合わせ内容は1000文字以内で入力してください");
+        }
+        else{
+            $("#content-validation").text("");
+
         }
     })
 });
