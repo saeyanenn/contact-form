b30cdfba05a706ff6f38a2250e8d372ddb87798b
ito sae <saeito20040122@gmail.com>
2022-05-21 17:43:35 +0900

    フォーム画面を表示できるようにした

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<?php
+include_once("head.php")
+?>
+
+<body>
+    <?php
+    include_once("header.php");
+    ?>
+    <main>
+        <?php
+        include_once("form.php")
+        ?>
+    </main>
+
+</body>
+
+</html>
