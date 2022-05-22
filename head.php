f435afc3660d224bbacb7b1be8b06b36d1e0f90f
ito sae <saeito20040122@gmail.com>
2022-05-22 19:58:23 +0900

    リファクタリング

--- head.php
+++ head.php
@@ -1,8 +1,3 @@
-<?php
-
-
-?>
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
 <html lang="ja">
 <?php
-include_once("head.php")
+include_once("head.php");
 ?>
 
 <body>
     <?php
+    $header_text = "お問い合わせフォーム";
     include_once("header.php");
     ?>
     <main>
         <?php
-        include_once("form.php")
+        include_once("form.php");
         ?>
     </main>
 
