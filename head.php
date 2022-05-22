bf8c6ff370290a5660f48deac19c3fdff2ea1d23
ito sae <saeito20040122@gmail.com>
2022-05-22 23:38:19 +0900

    アイコンを読み込めるようにした

--- head.php
+++ head.php
@@ -6,4 +6,5 @@
     <link rel="stylesheet" href="style.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="formValidation.js"></script>
+    <script src="https://kit.fontawesome.com/b978b35515.js" crossorigin="anonymous"></script>
 </head>
