3574a215f1296eee48f637a3dd0876a75cf89ba2
ito sae <saeito20040122@gmail.com>
2022-05-24 14:26:31 +0900

    console.logを削除

--- formValidation.js
+++ formValidation.js
@@ -95,7 +95,6 @@ $(function () {
   $("#content").on("blur", function (event) {
     const contentLength = event.target.value.length;
     $("#count-word").text(`＜${contentLength}文字＞`);
-    console.log(contentLength);
     if (contentLength > 1000) {
       $("#content-validation").text(
         "お問い合わせ内容は1000文字以内で入力してください"
